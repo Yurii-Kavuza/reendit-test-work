9ce6533f4d5b652b066628078f40fd143b08bb15
Yurii-Kavuza <kavuzaiurii@gmail.com>
2023-01-02 13:11:48 +0200

    clean app.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import { Container, Header, Logo, Link } from './App.styled';
-//import Box from './Box';
 import { Home } from '../pages/Home';
 import { Movies } from '../pages/Movies';
 import { NotFound } from 'pages/NotFound';
 import { GlobalStyle } from './GlobalStyle';
 import { Layout } from './Layout';
+import MovieDetails from './MovieDetails/MovieDetails';
 
 export const App = () => {
   return (
     <>
-      {/* <Container>
-        <Header>
-          <Logo>
-            <span role="img" aria-label="computer icon">
-              💻
-            </span>{' '}
-            GoMerch Store
-          </Logo>
-          <nav>
-            <Link to="/" end>
-              Home
-            </Link>
-            <Link to="/movies">Movies</Link>
-          </nav>
-        </Header>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId" element={<MovieDetails />} />
-        <Route path="/movies/:movieId/cast" element={<Cast />} />
-        <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        
-      </Container> */}
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="home" element={<div>Home</div>} />
+          <Route index element={<div>Home</div>} />
           <Route path="movies" element={<div>Movies</div>} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<div>Cast</div>} />
+            <Route path="reviews" element={<div>Review</div>} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
       <GlobalStyle />
