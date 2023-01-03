e5ffc5b0e63c6f5d85fe4ac64a263fc106f4ef42
Yurii-Kavuza <kavuzaiurii@gmail.com>
2023-01-03 23:47:50 +0200

    add links to film titles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,38 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 import { fetchTrendingMovies } from 'services/fetchMoviesApi';
 
+const NavItem = styled(NavLink)`
+  display: block;
+  padding: ${p => p.theme.space[3]}px;
+  text-decoration: none;
+  color: ${p => p.theme.colors.text};
+  border-radius: 8px;
+  &.active {
+    background-color: ${p => p.theme.colors.primary};
+    color: ${p => p.theme.colors.white};
+  }
+  :hover:not(.active) {
+    color: ${p => p.theme.colors.primary};
+  }
+`;
+
 export const Home = () => {
-  const [trendingMovies, setTrendingMovies] = useState([fetchTrendingMovies()]);
-  setTrendingMovies(fetchTrendingMovies());
-  console.log(trendingMovies);
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  useEffect(() => {
+    fetchTrendingMovies().then(setTrendingMovies);
+  }, []);
+
   return (
     <main>
       <h1>Trending today</h1>
       <ul>
-        {trendingMovies.map(movie => (
-          <li key={movie.id}>{movie.name ?? movie.title}</li>
+        {trendingMovies.map(({ id, name, title }) => (
+          <li key={id}>
+            <NavItem to={`movies/${id}`}>{name ?? title}</NavItem>
+          </li>
         ))}
       </ul>
     </main>
