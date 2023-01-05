d4bb181e688fb71bbc8819ceed6dc1b585307fa3
Yurii-Kavuza <kavuzaiurii@gmail.com>
2023-01-06 00:13:15 +0200

    fix go back when we don't have previous location

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -14,7 +14,7 @@ const navItems = [
 const MovieDetails = () => {
   const { movieId } = useParams();
   const location = useLocation();
-  const backLinkHref = location.state?.from;
+  const backLinkHref = location.state?.from ?? '/';
   console.log(location);
   console.log(backLinkHref);
   const [movie, setMovie] = useState();
@@ -58,7 +58,7 @@ const MovieDetails = () => {
       <Box p={4} mb={3} boxShadow="small">
         <div>Additional information</div>
         {navItems.map(({ href, text }) => (
-          <NavItem to={href} key={text} state={{ from:  backLinkHref}}>
+          <NavItem to={href} key={text} state={{ from: backLinkHref }}>
             {text}
           </NavItem>
         ))}
