0a425976fa29c03fd830e426fe3b5041cb09cdc9
Yurii-Kavuza <kavuzaiurii@gmail.com>
2023-03-12 21:51:59 +0000

    Deploying to gh-pages from @ Yurii-Kavuza/reendit-test-work@76f5ba14d350ba01c0983b7cbd48066138db2050 🚀

---
+++ static/js/273.dba8a2f7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreendit_test_work=self.webpackChunkreendit_test_work||[]).push([[273],{2273:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r=n(4476),s=n(1087),u=n(184),o=function(){return(0,u.jsxs)(r.Z,{display:"flex",alignItems:"center",justifyContent:"cemter",p:3,children:["Sorry, we could not help you. Please return to the\xa0",(0,u.jsx)(s.rU,{to:"/",children:"Home"})]})}}}]);
+//# sourceMappingURL=273.dba8a2f7.chunk.js.map
